5125f09be9c72373bcab0c182826ac905f7918aa
yumi1906 <yuwasa60@gmail.com>
2023-12-24 11:29:34 +0900

    りあクト 4章 4-2-3 p.158 リテラル型の話

--- 00/sample-enum.ts
+++ 00/sample-enum.ts
@@ -1,8 +1,24 @@
+// enum Pet {
+//     Cat,
+//     Dog,
+//     Rabbit,
+// }
+
+// console.log(Pet.Cat, Pet.Dog, Pet.Rabbit);
+// 実体は数値
+
 enum Pet {
-    Cat,
-    Dog,
-    Rabbit,
+    Cat = 'cat',
+    Dog = 'dog',
+    Rabbit = 'rabbit',
 }
 
-console.log(Pet.Cat, Pet.Dog, Pet.Rabbit);
-// 実体は数値
+// let Tom: Pet = Pet.Cat;
+// Tom = 'dog'; // Type '"dog"' is not assignable to type 'Pet'.
+
+let Tom: 'cat' = 'cat';
+// Tom = 'mouse'; // Type '"mouse"' is not assignable to type '"cat"'.
+
+let Mary: 'Cat' | 'Dog' | 'Rabbit' = 'Cat';
+Mary = 'Rabbit';
+// Mary = 'Parrot'; // Type '"Parrot"' is not assignable to type '"Cat" | "Dog" | "Rabbit"'.
