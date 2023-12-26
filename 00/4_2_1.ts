e5986ee58d9b9225e926d91bac8bb5efe4bb10fa
yumi1906 <yuwasa60@gmail.com>
2023-12-26 22:06:53 +0900

    ファイル整理

--- 00/4_2_1.ts
+++
@@ -1,2 +0,0 @@
-let n: number = 3;
-// n = 'foo'; 「error TS2322: Type 'string' is not assignable to type 'number'.」になる
--- 00/index-signature.ts
+++
@@ -1,9 +0,0 @@
-interface Status {
-    [parameter: string]: number;
-}
-
-const myStatus: Status = {
-    level: 22,
-    experience: 3058,
-    // job: 'maze', // Type 'string' is not assignable to type 'number'.
-}
